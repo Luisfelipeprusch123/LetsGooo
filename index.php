9c4bcc30e83908c430db391909894c6e7d33d2c9
AnaPaula520 <144857573+AnaPaula520@users.noreply.github.com>
2023-10-24 20:11:57 -0300

    correção de erro

--- index.php
+++ index.php
@@ -1,26 +1,28 @@
 <!DOCTYPE html>
-<html>
+<html lang="PT-BR">
 <head>
+<link rel="stylesheet" href="./css/style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calm your anxiety</title>
-    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <nav class="navbar">
+<div class="navbar">
         <div class="logo-title">
             <img src="images/logo.png" alt="Logo">
             <h1>Calm your anxiety</h1>
         </div>
-        
+
         <div class="search">
             <input type="search" placeholder="Pesquisar">
         </div>
-                <ul>
+
+        <div class="Menu">                
                 <li><a href="#">Chat Bot</a></li>
                 <li><a href="#">Postagem</a></li>
                 <li><a href="#">Publicação</a></li>
-                </ul>
-    </nav>
-        </body>
-    </html>
-    <!-- http://localhost/LetsGooo/index.php -->
+                </div>
     
+</body>
+</html>
+ <!-- http://localhost/LetsGooo/index.php -->
