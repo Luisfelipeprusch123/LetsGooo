c71baafdba8045cf0d49e5d41d53c19c849dfe70
AnaPaula520 <144857573+AnaPaula520@users.noreply.github.com>
2023-10-24 19:00:21 -0300

    Primeira alteração do projeto QI | TI S2

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Calm your anxiety</title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <nav class="navbar">
+        <div class="logo-title">
+            <img src="images/logo.png" alt="Logo">
+            <h1>Calm your anxiety</h1>
+        </div>
+        
+        <div class="search">
+            <input type="search" placeholder="Pesquisar">
+        </div>
+        <ul>
+                <li><a href="#">Chat Bot</a></li>
+                <li><a href="#">Postagem</a></li>
+                <li><a href="#">Publicação</a></li>
+            </ul>
+    </nav>
+
+</body>
+</html>
